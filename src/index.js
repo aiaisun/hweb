1ef224cd689e10c72868c3bf25e8dc44c3859ffb
aiaisun <yungaisun@gmail.com>
2020-06-16 16:44:20 +0800

    20200616b

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const utf8 = require('utf8');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const tz = require('moment-timezone');
-const moment = require ('moment');
+const moment = require('moment');
 const mongoClient = require('mongodb').MongoClient;
 const mongo = 'mongodb://localhost:27017/hweb'
 // const url = "mongodb://localhost:27017";
@@ -18,7 +18,7 @@ let mongodb = null
 //     if (err) throw err;
 //     console.log("mongodb is connected.")
 //     db.close()
-    
+
 // })
 // var url1 = "mongodb://localhost:27017/hweb";
 // client.connect(url1, {useUnifiedTopology: true}, function(err, db) {
@@ -36,7 +36,7 @@ let mongodb = null
 let { PythonShell } = require('python-shell');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
     saveUninitialized: false,
@@ -50,32 +50,44 @@ app.use(session({
 // set the storage engine
 const storage = multer.diskStorage({
     destination: 'public/tmpTLC/', //save path
-    filename: function (req, file, cb){
+    filename: function (req, file, cb) {
         cb(null, file.originalname);
     }
 });
 
 //init upload
-const upload = multer({storage: storage});//single image Key:myImage
+const upload = multer({ storage: storage });//single image Key:myImage
+
+//email system
+const nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'alisunlcfc@gmail.com',
+        pass: 'nbvmrperbmayaemk'
+    }
+})
+
+
 
 // member system//
 // Create a connect to MongoDB  Q 為什app要設在db 連接裡面
-mongoClient.connect(mongo, {useUnifiedTopology: true}, function (err, db) {
+mongoClient.connect(mongo, { useUnifiedTopology: true }, function (err, db) {
     if (err) return console.log(err)
     mongodb = db.db("hweb");
     // mongodb = db
-  
+
     // Start Service
     app.listen(3000, function () {
-      console.log('Server Starts')
+        console.log('Server Starts')
     })
-  })
+})
 
 // app.listen(3000, () => {
 //     console.log('Server starts')
 // })
 // const data = {}
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     // data.isLogined = !! req.session.loginUser;
     // data.loginUser = req.session.loginUser;
     // data.userEName = req.session.userEName;
@@ -85,21 +97,18 @@ app.get('/', (req, res) =>{
     res.render('home')
 });
 
-app.get('/user/trysession', (req, res)=>{
+app.get('/user/trysession', (req, res) => {
     req.session.views = req.session.views || 0;
     req.session.views++;
     res.contentType('text/plain');
     res.write('拜訪次數:' + req.session.views + "\n");
-    res.end( JSON.stringify(req.session));
+    res.end(JSON.stringify(req.session));
 
 })
 
 
-
-//login page
-
-//try new login
-app.get('/user/login', (req, res)=>{
+//////////////////// 登入 ////////////////////
+app.get('/user/login', (req, res) => {
     const data = {};
     const timeFormat = "YYYY-MM-DD HH:mm:ss";
     const mo1 = moment(new Date());
@@ -108,28 +117,28 @@ app.get('/user/login', (req, res)=>{
         delete req.session.flashMsg;
     }
 
-    data.isLogined = !! req.session.loginUser;
+    data.isLogined = !!req.session.loginUser;
     data.loginUser = req.session.loginUser;
     data.userEName = req.session.userEName;
     // console.log(data)
-    if (data.isLogined){//true
+    if (data.isLogined) {//true
         console.log(`${data.loginUser} 於 ${mo1.format(timeFormat)} 登入`)
     }
     res.render('login', data);
 })
 
 
-app.post('/user/login', (req, res)=>{
+app.post('/user/login', (req, res) => {
     const collection = mongodb.collection('users');
     //檢查有沒有這個帳號 
     let account = {
         'userAccount': req.body.userAccount,
-        'userPW' :req.body.password
+        'userPW': req.body.password
     };
-    collection.findOne(account, (err, document) =>{
-        if (document){ //true
+    collection.findOne(account, (err, document) => {
+        if (document) { //true
             console.log(document.userCName);
-            req.session.loginUser = req.body.userAccount; 
+            req.session.loginUser = req.body.userAccount;
             req.session.userEName = document.userEName
         } else { // false
             req.session.flashMsg = "帳號密碼錯誤";
@@ -139,17 +148,17 @@ app.post('/user/login', (req, res)=>{
     // res.redirect('/user/login'); 如果放這個 redirect之後session會被更新
 })
 
-//登出//
-app.get('/user/logout', (req, res)=>{
+//////////////////// 登出 ////////////////////
+app.get('/user/logout', (req, res) => {
     delete req.session.loginUser;
     res.redirect('/user/login')
 })
 
 //////////////////// 註冊流程 ////////////////////
-app.get('/user/register', (req, res) =>{
+app.get('/user/register', (req, res) => {
     res.render('register');
 });
-app.get('/user/registerSuccess', (req, res) =>{
+app.get('/user/registerSuccess', (req, res) => {
     res.render('registerSuccess');
 });
 
@@ -161,19 +170,19 @@ app.get('/user/registerSuccess', (req, res) =>{
 //     console.log(`${data.userEmail} 註冊成功`);
 //     next();
 //   }
-app.get('/try-finddb',(req,res)=>{
+app.get('/try-finddb', (req, res) => {
     const collection = mongodb.collection('users');
     const myemail = 'ali.sun@lcfuturecenter.com';
-    let emailData = {'userEmail': myemail};
-    collection.findOne({'userEmail': myemail}, (err, document) =>{
+    let emailData = { 'userEmail': myemail };
+    collection.findOne({ 'userEmail': myemail }, (err, document) => {
         console.log(document);
         // const data = document.name
         res.json(document);
     })
-    
+
 })
 
-app.post('/user/register', upload.single('tlcfile'), (req, res) =>{
+app.post('/user/register', upload.single('tlcfile'), (req, res) => {
     // console.log('user in session')
     // console.log(req.session);
     const collection = mongodb.collection('users');
@@ -181,47 +190,82 @@ app.post('/user/register', upload.single('tlcfile'), (req, res) =>{
     const mo1 = moment(new Date());
 
     //檢查有沒有這個帳號 有的話再insert     
-    let account = {'userAccount': req.body.userAccount};
-   
-    collection.findOne( account, (err, document) =>{
-        
-        if (document){
+    let account = { 'userAccount': req.body.userAccount };
+
+    collection.findOne(account, (err, document) => {
+
+        if (document) {
             console.log(`${document.userAccount} 嘗試重複註冊，但在 ${document.create_date}已註冊過帳號`);
-            res.json({alert: "帳號已存在"})//json字串過去一定要json格式再.出來
-        } else{
+            res.json({ alert: "帳號已存在" })//json字串過去一定要json格式再.出來
+        } else {
             let insertDate = {
-                'userCName'  : req.body.userCName,
-                'userEName'  : req.body.userEName,
+                'userCName': req.body.userCName,
+                'userEName': req.body.userEName,
                 'userAccount': req.body.userAccount,
-                'userEmail'  : req.body.userAccount + "@lcfuturecenter.com",
-                'userPW'     : req.body.userpassword,
+                'userEmail': req.body.userAccount + "@lcfuturecenter.com",
+                'userPW': req.body.userpassword,
                 'create_date': mo1.format(timeFormat),
             }
             collection.insertOne(insertDate, function (err, document) {
-              if (err) return res.json(err);
-          
-              console.log(`${account.userAccount} 註冊成功`);
-            //   res.redirect('registerSuccess');
-              res.json({alert: "帳號註冊成功"})
+                if (err) return res.json(err);
+
+                console.log(`${account.userAccount} 註冊成功`);
+                //   res.redirect('registerSuccess');
+                res.json({ alert: "帳號註冊成功" })
             })
         }
     })
-  })
+})
 
-//////////////////// 註冊流程 ////////////////////
+//////////////////// 忘記密碼 ////////////////////
+app.get('/user/forgetpw', (req, res) => {
+    res.render('forgetPW')
+})
 
+app.post('/user/forgetpw', upload.single('tlcfile'), (req, res) => {
+    const collection = mongodb.collection('users');
+    let account = {
+        'userAccount': req.body.userAccount
+    };
+    collection.findOne(account, (err, document) => {
+        if (document) { //true
+
+            let mailOptions = {
+                from: 'alisunlcfc@gmail.com',
+                to: `${account.userAccount}@lcfuturecenter.com`,
+                subject: "Here's your password!",
+                text: `密碼: ${document.userPW}`
+            };
+            console.log(`${account.userAccount} 忘記密碼`)
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(`密碼已成功寄送給 ${mailOptions.to}`);
+                    // console.log(info.response);
+                }
+            })
+            
+            res.json({ msg: "密碼已寄送" })
+        } else { // false
+            console.log(`${account.userAccount} 帳號不存在`);
+            res.json({ msg: "帳號不存在" })
+        }
+    })    
+})
 
 //////////////////// TLC 部分 ////////////////////
-app.get('/tlc', (req, res) =>{
+app.get('/tlc', (req, res) => {
     // res.render('tlc', data);
     res.render('tlc')
 })
 
-app.post('/tlc', upload.single('tlcfile'), (req, res) =>{
+app.post('/tlc', upload.single('tlcfile'), (req, res) => {
     console.log(req.file);//req.files 報錯
     const files = {
-        "data" : req.file,
-        "project" : req.body.project
+        "data": req.file,
+        "project": req.body.project
     }
     res.json(files);
 });
@@ -230,20 +274,20 @@ app.post('/tlc', upload.single('tlcfile'), (req, res) =>{
 app.get('/call/python', pythonProcess)
 
 function pythonProcess(req, res) {
-  let options = {
-    args:
-      [
-        req.query.name,
-        req.query.from
-      ]
-  }
+    let options = {
+        args:
+            [
+                req.query.name,
+                req.query.from
+            ]
+    }
 
-  PythonShell.run('./process.py', options, (err, data) => {
-    if (err) res.send(err)
-    const parsedString = JSON.parse(data)
-    console.log(`name: ${parsedString.Name}, from: ${parsedString.From}`)
-    res.json(parsedString)
-  })
+    PythonShell.run('./process.py', options, (err, data) => {
+        if (err) res.send(err)
+        const parsedString = JSON.parse(data)
+        console.log(`name: ${parsedString.Name}, from: ${parsedString.From}`)
+        res.json(parsedString)
+    })
 
 }
 
@@ -258,7 +302,7 @@ function pythonParseTLC(req, res) {
     let options = {
         args: [
             req.query.project,//接到前端網頁的project 跟檔案路徑
-            path       
+            path
         ]
     }
     // 因為python file 路徑填錯,所以一直報錯
@@ -296,7 +340,7 @@ function pythonParseVram(req, res) {
     let options = {
         args: [
             req.query.project,//接到前端網頁的project 跟檔案路徑
-            path       
+            path
         ]
     }
     // 因為python file 路徑填錯,所以一直報錯
@@ -311,16 +355,15 @@ function pythonParseVram(req, res) {
     })
 };
 
-//////////////////// TLC ////////////////////
 
 
-app.get('/user/test', (req, res) =>{
+app.get('/user/test', (req, res) => {
     res.render('test2');
 });
 
 
 
-app.post('/user/test', (req, res) =>{
+app.post('/user/test', (req, res) => {
     const testQQ = {
         "data": req.body
     }
@@ -338,7 +381,7 @@ app.post('/user/test', (req, res) =>{
 
 
 // 自定404 page
-app.use((req, res)=>{
+app.use((req, res) => {
     res.type('text/plain');
     res.status(404);
     res.send('404 - Page cannot found.');
